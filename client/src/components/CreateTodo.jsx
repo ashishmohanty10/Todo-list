ad287309396b07c5ffb64b770fff12f922e6f6eb
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-03-14 00:10:58 +0530

    done

--- client/src/components/CreateTodo.jsx
+++ client/src/components/CreateTodo.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const CreateTodo = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleAddFetch = async () => {
+    await axios
+      .post("http://localhost:3000/todo", {
+        title: title,
+        description: description,
+      })
+      .then(() => {
+        alert("Todo added");
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  };
   return (
     <div>
       <form action="" className="space-x-5">
-        <input type="text" placeholder="title" className="p-3 border " />
-        <input type="text" placeholder="description" className="p-3 border " />
+        <input
+          type="text"
+          placeholder="title"
+          className="p-3 border "
+          onChange={function (e) {
+            const value = e.target.value;
+            setTitle(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="description"
+          className="p-3 border "
+          onChange={function (e) {
+            const value = e.target.value;
+            setDescription(e.target.value);
+          }}
+        />
 
         <button
           type="submit"
+          onClick={handleAddFetch}
           className="px-4 py-2 text-sm font-bold text-white bg-slate-900"
         >
           Add Todo
--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todos = ({ todos }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
   return (
     <div className="my-5">
       {todos.map((todo, index) => {
@@ -12,8 +17,15 @@ const Todos = ({ todos }) => {
             <p className="text-sm font-medium text-blue-600">
               {todo.description}
             </p>
-            <button className="text-sm font-medium text-white bg-blue-600">
-              {todo.completed ? "TRUE" : "FALSE"}
+            <button
+              className="text-sm font-medium text-white bg-blue-600"
+              onClick={handleClick}
+            >
+              {clicked
+                ? todo.completed
+                  ? "false"
+                  : "true"
+                : "Click to reveal"}
             </button>
           </div>
         );
